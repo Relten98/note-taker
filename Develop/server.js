134f897baef6152827eb12d3c6a00754e4a8d616
Relten98 <diezhandinoty@gmail.com>
2021-02-02 19:20:10 -0800

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const apiroutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js");
 
+
+
 const app = express();
 
-/// Sets port to 8080
-const port = 8080;
+/// Sets port to 8081
+const port = 8081;
+
+
 
 app.use
