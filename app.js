faeb347e76b21e8730b09fc93aee5970ebad4f94
Relten98 <diezhandinoty@gmail.com>
2021-02-02 19:09:50 -0800

    new core

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-let 
+let noteText = getelementbyid."noteText"
+const bacon = "delicious"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let Ty = "wants2die"
+
+function willtolive() {
+    if(appfunctions = true) {
+        let Ty = "wants2live"
+    }
+} */
