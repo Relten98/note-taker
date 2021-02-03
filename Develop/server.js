478a8767a24d82d4cd159889541fa40de2e508cc
Relten98 <diezhandinoty@gmail.com>
2021-02-02 19:24:10 -0800

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,14 @@ const app = express();
 /// Sets port to 8081
 const port = 8081;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
+/// Routes
 
-app.use
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+
+/// Kicks this whole thing off, and initiates the server.
+app.listen(PORT, ()=> console.log(`Listening on Port : ${PORT}`))
